1e2319c8437dec2006e0d7d50d21e21ef84d1aff
LATerada <larissaakiterada@gmail.com>
2023-05-24 00:42:49 -0300

    :sparkles: Add PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,9 +1,25 @@
+import like from "../assets/upArrow.svg";
+import dislike from "../assets/downArrow.svg";
+import comment from "../assets/comment.svg";
+
 export const PostCard = (props) => {
-  const {post} = props
-  // console.log(post)
+  const { post } = props;
+  console.log(post);
   return (
-    <div className="w-80 border-gray-cardBorder border bg-gray-cardBg m-1">
-      {post.creator.name}
+    <div className="w-80 grid grid-rows-3 border-gray-cardBorder border bg-gray-cardBg m-1.5 font-ibm px-2.5 py-2 rounded-xl">
+      <p className="font-normal text-xs text-gray-inputPost">Send by {post.creator.name}</p>
+      <p className="flex flex-wrap font-normal text-lg text-black py-2">{post.postContent}</p>
+      <div  className="flex gap-2">
+        <button className="flex justify-center items-center border border-gray-cardBorder rounded-xl px-2 gap-4 h-7">
+          <img className="w-3.5" src={like}></img>
+          <span className="font-bold text-xxs">{post.likes - post.dislikes}</span>
+          <img className="w-3.5"s src={dislike}></img>
+        </button>
+        <button className="flex justify-center items-center border border-gray-cardBorder rounded-xl px-2 gap-4 h-7">
+          <img className="w-3.5" src={comment}></img>
+          <span className="font-normal text-xxs">{post.comments}</span>
+        </button>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      xxs: ["9px", "9.5px"],
+    },
     colors: {
       blue: "#4088CB",
       gray: {
@@ -9,7 +12,7 @@ export default {
         mid: "#A3A3A3",
         inputBorder: "#D5D8DE",
         inputText: "#323941",
-        inputPost:"#6F6F6F",
+        inputPost: "#6F6F6F",
         cardBg: "#FBFBFB",
         cardBorder: "#E0E0E0",
       },
@@ -19,6 +22,7 @@ export default {
       },
       rose: "#FF6489",
       red: "#e11d48",
+      black:"#000000",
       white: "#FFFFFF",
     },
     fontFamily: {
